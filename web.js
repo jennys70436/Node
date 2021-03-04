dd6495c157be5167ae6820fe20c536e9b0d49063
jennys70436 <jennys70436@hotmail.com>
2021-03-04 22:08:34 +0800

    server監聽後回傳文字、html

--- web.js
+++ web.js
@@ -1,6 +1,13 @@
 var http = require('http')
+var fs = require('fs');
 var server = http.createServer(function (request, response) {
-    response.write('Hello')
+    // 帶入status Code及Header屬性, 放在最上面
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+    var html = fs.readFileSync('./index.html');
+    response.write(html)
+    // response.write('Hello World') // 直接輸出文字
     response.end() // 傳輸完畢
 })
 server.listen(3000, '127.0.0.1') // 監聽3000 port
