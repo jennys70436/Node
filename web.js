a77799b5339e0acb65750e6f0ccd1aefc400c8f7
Jenny <jennys70436@hotmail.com>
2021-03-07 22:52:17 +0800

    完成

--- web.js
+++ web.js
@@ -1,13 +1,39 @@
-var http = require('http')
-var fs = require('fs');
-var server = http.createServer(function (request, response) {
-    // 帶入status Code及Header屬性, 放在最上面
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const mime = require('mime')
+const server = http.createServer(function (request, response) {
+    const myUrl = new URL(`http://127.0.0.1:3000${request.url}`)
+    console.log(myUrl.pathname)
+    var relativePathname = decodeURIComponent(myUrl.pathname);
+    if (myUrl.pathname.endsWith('/')){
+        myUrl.pathname += 'index.html'
+    } 
+    fs.stat(`.${relativePathname}`, function(err, stats){
+        if(!err && stats.isDirectory()) {
+            response.writeHead(302, {
+                'Location': myUrl.pathname + '/' + (myUrl.search || "")
+            })
+            response.end();
+            return;
+        }
+        if(!err && stats.isFile()) {            
+            // 非同步 
+            fs.readFile(`.${relativePathname}`, function(err, html){
+                if(!err){
+                    // 帶入status Code及Header屬性, 放在最上面
+                    response.writeHead(200, {
+                        'Content-Type': mime.getType(myUrl.pathname)
+                    })
+                    response.write(html)
+                    response.end()
+                }
+            });
+        } else {
+            response.writeHead(404)
+            response.write('NOT FOUND')
+            response.end()
+        }
     })
-    var html = fs.readFileSync('./index.html');
-    response.write(html)
-    // response.write('Hello World') // 直接輸出文字
-    response.end() // 傳輸完畢
 })
 server.listen(3000, '127.0.0.1') // 監聽3000 port
